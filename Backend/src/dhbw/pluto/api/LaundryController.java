cd1cb8ebbc61ce78ec090a621933b66c582d13b6
Felix Seidel <felix@gnampfelix.de>
2016-10-19 14:19:20 +0200

    Add laundry icon handling
    The icons are available under ".../laundry/icons".

--- Backend/src/dhbw/pluto/api/LaundryController.java
+++ Backend/src/dhbw/pluto/api/LaundryController.java
@@ -1,11 +1,18 @@
 package dhbw.pluto.api;
 
+import java.util.List;
+
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.core.Response;
 
+import org.json.JSONArray;
+
+import dhbw.pluto.laundry.IconLoadingException;
 import dhbw.pluto.laundry.LaundryAlert;
+import dhbw.pluto.laundry.LaundryIcon;
+import dhbw.pluto.laundry.LaundryIconLoader;
 
 
 @Path("/laundry")
@@ -18,6 +25,26 @@ public class LaundryController {
 		System.out.println("Eingegebene Zeit: " + time);
 		alert.initializeAlert();
 		return Response.status(200).entity("Alarm erstellt").build();
+	}
+	
+	@GET
+	@Path("/icons")
+	public Response listIcons() {
+		JSONArray result;
+		try {
+			result = convertIcons(LaundryIconLoader.loadLaundyIcons());
+		} catch(IconLoadingException e) {
+			return Response.status(501).build();
+		}
+		return Response.status(200).entity(result.toString()).build();
+	}
+	
+	private JSONArray convertIcons(List<LaundryIcon> icons) {
+		JSONArray result = new JSONArray();
+		for(int i = 0; i < icons.size(); i++) {
+			result.put(icons.get(i).toJSON());
+		}
+		return result;
 	}
 	
 
