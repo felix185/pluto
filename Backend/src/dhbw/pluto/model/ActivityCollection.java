22d57bd2b4a184dad9ce36b5610174b2a83d63a4
Felix Seidel <felix@gnampfelix.de>
2016-12-06 11:48:22 +0100

    Refacotr List<Activity>
    Create new Collection class: ActivityCollection

---
+++ Backend/src/dhbw/pluto/model/ActivityCollection.java
@@ -0,0 +1,21 @@
+package dhbw.pluto.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.json.JSONArray;
+
+import dhbw.pluto.controller.exception.ActivityLoadingException;
+import dhbw.pluto.model.actvities.Activity;
+
+public class ActivityCollection extends ArrayList<Activity> {
+	
+	public JSONArray toJSON() throws ActivityLoadingException {
+		JSONArray result = new JSONArray();
+		
+		for(int i = 0; i < super.size(); i++) {
+			result.put(super.get(i).toJSON());
+		}		
+		return result;
+	}
+}
