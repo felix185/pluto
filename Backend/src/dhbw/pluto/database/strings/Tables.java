528b1994f5e89d7b7334fa3c84456bc4ae664a64
Alex <toksick89@gmail.com>
2016-12-06 12:26:31 +0100

    added string constants for database.

---
+++ Backend/src/dhbw/pluto/database/strings/Tables.java
@@ -0,0 +1,16 @@
+package dhbw.pluto.database.strings;
+
+public class Tables {
+	
+	public static String DB = "pluto.db";
+	
+	public static String RECIPE_VIEW = "RecipeView";
+	public static String RECIPES = "Recipes";
+	public static String INGREDIENTS = "Ingredients";
+	public static String RECIPE_INGREDIENTS = "recipes_ingredients";
+	public static String LAUNDRY_ICONS = "laundryIcons";
+	public static String TAGS = "tags";
+	public static String ICON_TAGS = "iconTags";
+	public static String LAUNDRY_ICON_VIEW = "laundryIconView";
+	
+}
